798e1ab3e964a940172c8194f4018a57bfb63dbc
baizch <francobaisch00@gmail.com>
2024-08-09 17:37:09 -0300

    footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import React from 'react';
+
+import Footer from './components/footer/Footer';
+
 import './App.css';
 
 function App() {
   return (
-    <>
+    <div>
       <p className='text-red-500 p-8'>teste</p>
-    </>
+
+      <Footer />
+    </div>
   );
 }
 
