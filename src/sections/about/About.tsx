e40ee00e9b1c698d450976fadb027bbeccfdae33
baizch <francobaisch00@gmail.com>
2024-08-12 13:09:42 -0300

    modified text color and technologies display

--- src/sections/about/About.tsx
+++ src/sections/about/About.tsx
@@ -3,24 +3,17 @@ import React from 'react';
 const About: React.FC = () => (
   <section
     id='sobre'
-    className='-mt-40 h-screen flex flex-col justify-center items-center bg-white p-8'
+    className='-mt-64 h-screen flex flex-col justify-center items-center bg-white p-8'
   >
     <div className='max-w-3xl text-center'>
-      <h2 className='text-4xl font-bold text-amber-400 mb-4'>Sobre Mim</h2>
-      <p className='text-lg text-amber-500 leading-relaxed mb-6'>
+      <h2 className='text-4xl font-bold text-black mb-4'>Sobre Mim</h2>
+      <p className='text-lg text-black leading-relaxed mb-6'>
         Sou um desenvolvedor apaixonado por tecnologia e inovação, sempre em
-        busca de novos conhecimentos e desafios. Minha formação acadêmica em{' '}
-        <strong>Desenvolvimento de Software</strong> me forneceu uma base
-        sólida, mas é a minha curiosidade e vontade de aprender que me
-        impulsionam a continuar evoluindo.
+        busca de novos conhecimentos e desafios. Sou formado em{' '}
+        <strong>Análise e Desenvolvimento de Sistemas</strong> com 3 anos de
+        experiência profissional.
       </p>
-      <p className='text-lg text-amber-500 leading-relaxed mb-6'>
-        Acredito que a educação contínua é essencial para o crescimento, tanto
-        profissional quanto pessoal. Adoro explorar novas tecnologias,
-        frameworks e ferramentas que me ajudem a criar soluções mais eficientes
-        e criativas.
-      </p>
-      <p className='text-lg text-amber-500 leading-relaxed mb-6'>
+      <p className='text-lg text-black leading-relaxed mb-6'>
         Se você tem uma ideia ou projeto em mente, entre em contato! Vamos bater
         um papo e transformar essa ideia em realidade.
       </p>
--- src/sections/technologies/Technologies.tsx
+++ src/sections/technologies/Technologies.tsx
@@ -1,35 +1,107 @@
 import React from 'react';
+import { Card, Row, Col } from 'antd';
 
-const Technologies: React.FC = () => (
-  <section id='tecnologias' className='bg-white py-12'>
-    <div className='max-w-5xl mx-auto px-4'>
-      <div className='border-t-4 border-sky-200 mb-8'></div>
+const { Meta } = Card;
 
-      <h2 className='text-3xl font-bold text-amber-400 mb-8 text-center'>
-        Tecnologias
-      </h2>
+const Technologies: React.FC = () => {
+  interface ITechnologies {
+    name: string;
+    icon: string;
+  }
 
-      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8'>
-        <p className='text-lg font-semibold text-amber-500'>JavaScript</p>
-        <p className='text-lg font-semibold text-amber-500'>TypeScript</p>
-        <p className='text-lg font-semibold text-amber-500'>Java</p>
-        <p className='text-lg font-semibold text-amber-500'>React</p>
-        <p className='text-lg font-semibold text-amber-500'>React Native</p>
-        <p className='text-lg font-semibold text-amber-500'>Redux</p>
-        <p className='text-lg font-semibold text-amber-500'>Next.js</p>
-        <p className='text-lg font-semibold text-amber-500'>TailwindCSS</p>
-        <p className='text-lg font-semibold text-amber-500'>SASS</p>
-        <p className='text-lg font-semibold text-amber-500'>Node.js</p>
-        <p className='text-lg font-semibold text-amber-500'>Express.js</p>
-        <p className='text-lg font-semibold text-amber-500'>Nest.js</p>
-        <p className='text-lg font-semibold text-amber-500'>MySQL</p>
-        <p className='text-lg font-semibold text-amber-500'>PostgreSQL</p>
-        <p className='text-lg font-semibold text-amber-500'>
-          Docker & Docker Compose
-        </p>
+  const technologies: ITechnologies[] = [
+    {
+      name: 'JavaScript',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+    },
+    {
+      name: 'TypeScript',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
+    },
+    {
+      name: 'Java',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg',
+    },
+    {
+      name: 'React',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+    },
+    {
+      name: 'React Native',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg', // React Native pode usar o mesmo ícone do React
+    },
+    {
+      name: 'Redux',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg',
+    },
+    {
+      name: 'Next.js',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg',
+    },
+    {
+      name: 'TailwindCSS',
+      icon: 'https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/tailwindcss/tailwindcss-original.svg ',
+    },
+    {
+      name: 'SASS',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg',
+    },
+    {
+      name: 'Node.js',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg',
+    },
+    {
+      name: 'Express.js',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg',
+    },
+    {
+      name: 'Nest.js',
+      icon: 'https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/nestjs/nestjs-original.svg',
+    },
+    {
+      name: 'MySQL',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg',
+    },
+    {
+      name: 'PostgreSQL',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg',
+    },
+    {
+      name: 'Docker & Docker Compose',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg',
+    },
+  ];
+
+  return (
+    <section id='tecnologias' className='bg-white py-12'>
+      <div className='max-w-5xl mx-auto px-4'>
+        <div className='border-t-4 border-sky-200 mb-8'></div>
+
+        <h2 className='text-3xl font-bold text-black mb-8 text-center'>
+          Tecnologias
+        </h2>
+
+        <Row gutter={[16, 16]} justify='center'>
+          {technologies.map((tech, key) => (
+            <Col xs={24} sm={12} md={8} lg={6} key={key}>
+              <Card
+                hoverable
+                cover={
+                  <img
+                    alt={tech.name}
+                    src={tech.icon}
+                    style={{ padding: '20px', height: '100px' }}
+                  />
+                }
+              >
+                <Meta title={tech.name} />
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Technologies;
